fe267a26ff99b7f7e38a03fecab72c7f8e5060b5
Sarasmishra <Sarasmishra8100@gmail.com>
2025-12-30 17:57:32 +0530

    update cors policy

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,12 +5,8 @@ import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors({ origin: true, credentials: true }));
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
