cb51c86aa30b7560f8d0adaade9bc8e8669558fa
Sarasmishra <Sarasmishra8100@gmail.com>
2025-12-31 08:00:32 +0530

    fix slow cors preflight by handling options proprly

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,8 +4,29 @@ import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://purplemerit-user-management-fronten.vercel.app"
+];
 
-app.use(cors({ origin: true, credentials: true }));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // allow requests with no origin (Postman, server-to-server)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS not allowed"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.options("*", cors());
 
 app.use(express.json());
 
